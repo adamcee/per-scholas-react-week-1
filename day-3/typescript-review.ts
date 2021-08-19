78ca5838eb67c7e123fbd6d1930ec2398dad98a1
adamcee <adam.cahan@gmail.com>
2021-08-19 12:39:07 -0500

    Add src dir

--- day-3/typescript-review.ts
+++
@@ -1,9 +0,0 @@
-function hello(name: string, isSunny: boolean) {
-  if (!isSunny) {
-    return "Hi, " + name;
-  }
-
-  return `Hi, ${name}, it is sunny out today!`;
-}
-
-console.log(hello("Alice", true));
---
+++ src/hi.ts
@@ -0,0 +1,4 @@
+let name = "hi";
+console.log(name);
+
+export {};
